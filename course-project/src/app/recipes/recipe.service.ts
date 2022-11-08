8eb2187e0eb9d8867e6e506fc55adb8118a65c87
Bart GAUQUIE <bart.gauquie@sfpd.fgov.be>
2022-11-08 16:36:20 +0100

    section 14 : course project - observables

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,4 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
@@ -8,10 +7,9 @@ export class RecipeService {
         new Recipe(`A Test Recipe`, `This is simply a test`, `https://www.organicauthority.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTU5MzI5NjY4NDE2Njc3MDc4/avocado-toast.webp`, [new Ingredient('Test ingredient 1', 1), new Ingredient(`test ingredient 2`, 11)]),
         new Recipe(`Another Test Recipe`, `This is simply another test`, `https://www.organicauthority.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTU5MzI5NjY4NDE2Njc3MDc4/avocado-toast.webp`, [new Ingredient('another test ingredient', 33)])
     ];
-    
+
     getRecipes() {
         return this.recipes.slice(); // return a copy (slice())
     }
-    
 
 }

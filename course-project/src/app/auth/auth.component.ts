70f071694a5ab3e9440bb4d0b1bcd16946b5e36f
Bart GAUQUIE <bart.gauquie@sfpd.fgov.be>
2022-11-21 09:43:03 +0100

    section 21 : dynamic components + programmatically create components

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,17 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, ComponentFactoryResolver, OnDestroy, ViewChild} from "@angular/core";
 import {NgForm} from "@angular/forms";
 import {AuthService} from "./auth.service";
 import {Router} from "@angular/router";
+import {AlertComponent} from "../shared/alert/alert.component";
+import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
 
     isLoginMode = true;
     isLoading = false;
-    error?: string = undefined;
+    error?: null;
+    @ViewChild(PlaceholderDirective, {static: false}) alertHost!: PlaceholderDirective;
+    private closeSub!: Subscription; 
 
     constructor(private authService: AuthService,
                 private router: Router) {
@@ -40,9 +45,35 @@ export class AuthComponent {
                 this.router.navigate(['/recipes'])
             }, errorResponse => {
                 console.log(errorResponse);
+                
                 this.error = errorResponse;
+                this.showErrorAlert(errorResponse);
                 this.isLoading = false;
             });
         authForm.reset();
+    }
+
+    onHandleError() {
+        this.error = null;
+    }
+    
+    private showErrorAlert(message: string) {
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear();
+        
+        var alertComponentRef = hostViewContainerRef.createComponent(AlertComponent);
+        
+        alertComponentRef.instance.message = message;
+        this.closeSub = alertComponentRef.instance.close
+            .subscribe(() => {
+                this.closeSub.unsubscribe();
+                hostViewContainerRef.clear();
+            });
+    }
+    
+    ngOnDestroy() {
+        if(this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
     }
 }

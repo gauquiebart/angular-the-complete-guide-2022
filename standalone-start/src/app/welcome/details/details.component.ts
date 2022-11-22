eca7231d1d6ec1fea712d6e3c2023ca0dc6a3160
Bart GAUQUIE <bart.gauquie@sfpd.fgov.be>
2022-11-22 11:16:24 +0100

    section 24: standalone components

---
+++ standalone-start/src/app/welcome/details/details.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {AnalyticsService} from 'src/app/shared/analytics.service';
+import {HighlightDirective} from "../../shared/highlight.directive";
+
+@Component({
+    standalone: true,
+    selector: 'app-details',
+    templateUrl: './details.component.html',
+    styleUrls: ['./details.component.css'],
+    imports: [
+        HighlightDirective
+    ]
+})
+export class DetailsComponent {
+    constructor(private analyticsService: AnalyticsService) {
+    }
+
+    onClick() {
+        this.analyticsService.registerClick();
+    }
+
+}
